e60209aa673c2f3e00f381d25e88ff72f1c43a51
samir <samir.bas24@gmail.com>
2021-07-01 22:39:16 +0200

    Create Entity

---
+++ src/Entity/Operation.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OperationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OperationRepository::class)
+ */
+class Operation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $account_id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $operation_date;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $operation_type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAccountId(): ?int
+    {
+        return $this->account_id;
+    }
+
+    public function setAccountId(int $account_id): self
+    {
+        $this->account_id = $account_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOperationDate(): ?\DateTimeInterface
+    {
+        return $this->operation_date;
+    }
+
+    public function setOperationDate(\DateTimeInterface $operation_date): self
+    {
+        $this->operation_date = $operation_date;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getOperationType(): ?string
+    {
+        return $this->operation_type;
+    }
+
+    public function setOperationType(string $operation_type): self
+    {
+        $this->operation_type = $operation_type;
+
+        return $this;
+    }
+}
