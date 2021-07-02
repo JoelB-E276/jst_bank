9d16b5d7aa8dccd59ddf38edb83c87dc0ef69a5c
JoelB-E276 <joel.beauroy@outlook.fr>
2021-07-02 15:17:26 +0200

    before drop tables

--- src/Controller/AddAccountController.php
+++ src/Controller/AddAccountController.php
@@ -5,6 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
+use App\Entity\Account;
+use App\Entity\AccountType;
+use App\Entity\Operation;
+
+
 
 class AddAccountController extends AbstractController
 {
@@ -16,10 +24,18 @@ class AddAccountController extends AbstractController
         ]);
     }
 
-    public function addAccount()
+    #[Route('/', name: 'index')]
+    #[Route('/home', name: 'home')]
+    public function getAccounts(int $id=1, User $user): Response
     {
+        $user = New User;
+        var_dump($user);
+        $getAccounts = $this->getUser()->getAccountNumber()->getName();
+
+        return $this->render('front/index.html.twig' ,[
+            "getAccounts" => $getAccounts
+        ]);
 
     }
-
 
 }
