efd47808d67668c7cb3b8e00e1c0789ff25ec234
Joel BE276 <joel.beauroy@outlook.fr>
2021-07-07 16:44:20 +0200

    thomas cleaning

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -52,7 +52,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
  
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('index'));    
+        return new RedirectResponse($this->urlGenerator->generate('account_index'));    
     }
  
     protected function getLoginUrl(Request $request): string
