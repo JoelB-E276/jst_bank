e570e9367da171c4a04ad1d36c549c233fbd9553
samir <samir.bas24@gmail.com>
2021-07-09 08:51:25 +0200

    Améliorations Account

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -2,23 +2,23 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+// use App\Entity\User;
 use App\Entity\Account;
 use App\Entity\AccountGroup;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AccountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('account_number')
-            ->add('created_at')
-            ->add('amount')
-            ->add('accountgroup', EntityType::class, [
+            ->add('account_number', TextType::class, ['label' => 'N° Compte'])
+            ->add('amount', TextType::class, ['label' => 'Solde'])
+            ->add('accountgroup', EntityType::class, ['label' => 'Type de compte',
                 'class' => AccountGroup::class,
                 'choice_label' => 'name'
             ])
